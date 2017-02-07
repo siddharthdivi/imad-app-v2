c46faf6ec01124792cd3ec80d4221d087253468b
siddharthdivi <siddhudivi@gmail.com>
2017-02-07 18:06:43 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,5 +1,5 @@
 console.log('Loaded!');
 
 // change the value of id="main-text"
-var element = document.getElementByID('main-text');
+var element = document.getElementById('main-text');
 element.innerHTML = "New Value";
