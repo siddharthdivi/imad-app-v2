2d73169567a6853be9a5a97789acb97a085ee637
siddharthdivi <siddhudivi@gmail.com>
2017-02-10 18:50:34 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -24,19 +24,36 @@ button.onclick = function() {
 };
 
 //Submit Name
-var nameInput = document.getElementById('name');
-var name = nameInput.value;
 var submit = document.getElementById('submit_btn');
 submit.onclick = function() {
   //Make a request to the server and send the name
   
-  //Capture a list of names and render as a list
-  var names = ['name1','name2','name3','name4']
-  var list='';
-  for(var i=0;i<names.length;i++){
-      list += '<li>' + names[i] + '</li>';
-  }
-  var ul = document.getElementById('namelist');
-  ul.innerHTML = list;
+ //Create a request object.
+ var request = new XMLHttpRequest();
+ 
+ //Capture the response and store it in a variable.
+ request.onreadystatechange = function() {
+     if(request.readyState == XMLHttpRequest.DONE){
+         //Take some action
+         if(request.status == 200) {  //Successful Response
+          //Capture a list of names and render as a list
+              var names = request.responseText;
+              names = JSON.parse(names); //Convert from string to array
+              var list='';
+              for(var i=0;i<names.length;i++){
+                  list += '<li>' + names[i] + '</li>';
+              }
+              var ul = document.getElementById('namelist');
+              ul.innerHTML = list;
+              
+          }
+     }
+ };
+ 
+var nameInput = document.getElementById('name');
+var name = nameInput.value;
+ //Make the request
+ request.open('GET','http://siddharthdivi.imad.hasura-app.io/submit-name?name=' + name,true);
+ request.send(null);
   
 };
