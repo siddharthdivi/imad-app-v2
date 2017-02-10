5e9f043e7252c9d8e1ca28559124109ef2d87e7b
siddharthdivi <siddhudivi@gmail.com>
2017-02-10 18:35:09 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -100,6 +100,20 @@ app.get('/counter',function(req,res) {
    res.send(counter.toString());
 });
 
+
+var names=[];
+app.get('/submit-name', function (req, res) { //URL : /submit-name?name=xxxx
+  //Get the names from the request
+  var name = req.query.name; //TODO
+  
+  names.push(name);
+  //JSON : JavaScript Object Notation.
+  //A way of converting Javascript Objects into Strings
+  
+  res.send(JSON.stringify(names)); 
+});
+
+
 app.get('/:articleName', function (req, res) {
   //articleName == article-one
   //articles[articleName] == {} content object for article-one
@@ -117,18 +131,6 @@ app.get('/ui/main.js', function (req, res) {
 
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
-});
-
-var names=[];
-app.get('/submit-name/:name', function (req, res) {
-  //Get the names from the request
-  var name = req.params.name; //TODO
-  
-  names.push(name);
-  //JSON : JavaScript Object Notation.
-  //A way of converting Javascript Objects into Strings
-  
-  res.send(JSON.stringify(names)); 
 });
 
 
